a368af5e313c1c6b87b7db314235443bdea759eb
elaine <elaine-me@elaines-MacBook-Pro.local>
2019-01-09 16:21:53 +0000

    artist.rb

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,7 +2,20 @@ class Artist < ActiveRecord::Base
   has_many :songs
   has_many :genres, through: :songs
 
-  def slugify
-    
+def slug
+    name = self.name.gsub!(/[^0-9A-Za-z]/,' ')
+    name.downcase.gsub(" ","-")  
   end
+
+def self.find_by_slug(slug)
+ array = []
+ a = slug.gsub("-"," ")
+    hash.each do |k, v|
+      if hash[k] == "name" &&  /#{a}/.match(hash[v])
+        array << v
+      end
+    end 
+    binding.pry
+end
+
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -2,4 +2,10 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   has_many :song_genres
   has_many :genres, through: :song_genres
+
+  def slug
+    name = self.name.gsub!(/[^0-9A-Za-z]/,' ')
+    name.downcase.gsub(" ","-")  
+  end
+  
 end
