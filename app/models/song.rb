158746e605bc8f16a540c33526022cde82af118a
seenevz <serneves@hotmail.com>
2019-01-09 16:14:18 +0000

    added slug method

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,4 +2,17 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   has_many :song_genres
   has_many :genres, through: :song_genres
+
+  def slug
+    name = self.name.gsub!(/[^0-9A-Za-z]/, ' ')
+
+    name.downcase.gsub(" ", "-")
+  end
+
+  def self.find_by_slug(slug)
+    slugish = slug.gsub("-", " ").split
+    name = slugish.map {|n| n.capitalize}.join(" ")
+    Song.find_by(name: name)
+  end
+
 end
