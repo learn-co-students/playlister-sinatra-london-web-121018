4c74b18726d181678d37bb20202cdfe91647e47d
seenevz <serneves@hotmail.com>
2019-01-09 17:34:15 +0000

    created artist controller3

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,2 +1,12 @@
 class ArtistsController < ApplicationController
+
+     get "/artists" do
+        @artists = Artist.all
+
+        erb :"artist/index"
+     end
+
+     get "/artist/:slug" do
+        erb :"/artist/#{params[:id]}"
+     end
 end
--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,3 +1,18 @@
 class Genre < ActiveRecord::Base
-  has_many :songs, through: :song_genre
+  has_many :song_genres
+  has_many :songs, through: :song_genres
+  has_many :artists, through: :songs
+
+  def slug
+    name = self.name.gsub!(/[^0-9A-Za-z]/, ' ')
+
+    name.downcase.gsub(" ", "-")
+  end
+
+  def self.find_by_slug(slug)
+    slugish = slug.gsub("-", " ").split
+    name = slugish.map {|n| n.capitalize}.join(" ")
+    Genre.find_by(name: name)
+
+  end
 end
