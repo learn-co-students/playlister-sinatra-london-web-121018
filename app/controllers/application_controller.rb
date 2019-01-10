f21955bcf0b83b8087bba6e5cb7379518d7f73cc
timtan93 <timtan93@gmail.com>
2019-01-10 14:08:31 +0000

    All tests passed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,11 @@
 class ApplicationController < Sinatra::Base
+  require 'rack-flash'
   register Sinatra::ActiveRecordExtension
   set :session_secret, "my_application_secret"
   set :views, Proc.new { File.join(root, "../views/") }
+  enable:sessions
+  use Rack::Flash
+
 
   get '/' do
     erb :index
@@ -20,20 +24,40 @@ class ApplicationController < Sinatra::Base
 
   get '/songs/:slug' do
     @song = Song.find_by_slug(params[:slug])
- 
+
     erb :'songs/show'
   end
 
   post '/songs' do
-    @artist = Artist.create(name: params["song"]["artist"])
-    @genre = Genre.find(params["genres"].first.to_i) 
+    @artist = Artist.find_or_create_by(name: params["song"]["artist"])
+    @genre = Genre.find(params["genres"].first.to_i)
     @song = Song.create(name: params["song"]["name"], artist: @artist)
+
     @song.genres << @genre
-    
-    "hello"
+
+
+    flash[:message] = "#{@song.name},#{@song.artist.name},#{@song.genres.first.name}, Successfully created song."
 
     redirect "/songs/#{@song.slug}"
   end
+
+    get '/songs/:slug/edit' do
+      @song = Song.find_by_slug(params[:slug])
+        @genres = Genre.all
+      erb :'/songs/edit'
+    end
+
+    patch '/songs' do
+
+      @song = Song.find(params["song"]["id"])
+      @artist = @song.artist
+      @artist.name = params["song"]["artist"]
+
+
+
+      flash[:message] = "Successfully updated song.#{@song.name},#{@song.artist.name} "
+      redirect :"/songs/#{@song.slug}"
+    end
 
   get '/artists' do
     @artists = Artist.all
@@ -43,9 +67,12 @@ class ApplicationController < Sinatra::Base
 
   get '/artists/:slug' do
     @artist = Artist.find_by_slug(params[:slug])
- 
+
     erb :'artists/show'
   end
+
+
+
 
 
   get '/genres' do
@@ -56,7 +83,7 @@ class ApplicationController < Sinatra::Base
 
   get '/genres/:slug' do
     @genre = Genre.find_by_slug(params[:slug])
- 
+
     erb :'genres/show'
   end
 
